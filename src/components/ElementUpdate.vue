cef7cd123e75391fffe4dedc99bb836ea910db37
zhouyixun <291028775@qq.com>
2021-10-09 00:54:46 +0800

    save_element_filter

--- src/components/ElementUpdate.vue
+++ src/components/ElementUpdate.vue
@@ -42,6 +42,7 @@ const upload = (content) => {
           ElMessage.success({
             message: resp['message'],
           });
+          element.value.eleValue = resp['data']
         }
       });
 }
