f5d94ce5f59fee319f0614bf897c9c6b1d120ea5
JayWenStar <1599603313@qq.com>
2022-01-24 20:24:46 +0800

    ⭐️: 远控步骤中断

--- src/components/StepList.vue
+++ src/components/StepList.vue
@@ -12,9 +12,10 @@ const props = defineProps({
   projectId: Number,
   platform: Number,
   isShowRun: Boolean,
-  isDriverFinish: Boolean
+  isDriverFinish: Boolean,
+  debugLoading: Boolean
 })
-const emit = defineEmits(['runStep'])
+const emit = defineEmits(['runStep', 'stopStep'])
 const dialogVisible = ref(false)
 const stepId = ref(0)
 watch(dialogVisible, (newValue, oldValue) => {
@@ -89,6 +90,9 @@ const getStepsList = () => {
 const runStep = () => {
   emit('runStep')
 }
+const stopStep = () => {
+  emit('stopStep')
+}
 onMounted(() => {
   getStepsList();
 })
@@ -105,6 +109,7 @@ onMounted(() => {
                  @click="runStep">
         开始运行
       </el-button>
+      <el-button type="danger" size="mini" @click="stopStep" :disabled="!debugLoading">强制终止</el-button>
       <el-button type="primary" size="mini" @click="addStep">新增步骤</el-button>
     </el-button-group>
   </div>
