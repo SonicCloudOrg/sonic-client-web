6c9e7c0647a1e5fe26070cc9783b956f0e080c24
ZhouYixun <291028775@qq.com>
2022-03-17 21:16:03 +0800

    change_chunk

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
-import { defineConfig } from 'vite';
-import { join } from 'path';
+import {defineConfig} from 'vite';
+import {join} from 'path';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': join(__dirname, 'src'),
+    plugins: [vue()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
     },
-  },
+    resolve: {
+        alias: {
+            '@': join(__dirname, 'src'),
+        },
+    },
 });
