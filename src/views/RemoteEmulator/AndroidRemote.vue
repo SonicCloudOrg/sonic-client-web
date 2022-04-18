8002189903eae62992d4d3ddf448b83ca122ac30
ZhouYixun <291028775@qq.com>
2022-04-19 00:01:03 +0800

    Add some headers

--- src/views/RemoteEmulator/AndroidRemote.vue
+++ src/views/RemoteEmulator/AndroidRemote.vue
@@ -1,20 +1,20 @@
-
-  Copyright (C) [SonicCloudOrg] Sonic Project
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-         http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-
 <script setup>
+/*
+ *  Copyright (C) [SonicCloudOrg] Sonic Project
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ */
 import {useRoute, useRouter} from 'vue-router';
 import {computed, nextTick, onBeforeUnmount, onMounted, ref, watch} from 'vue';
 import {useStore} from 'vuex';
@@ -101,41 +101,41 @@ const agent = ref({});
 const screenUrls = ref([]);
 const uploadUrl = ref('');
 const text = ref({content: ''});
-const pocoTypeList =  ref([
+const pocoTypeList = ref([
   {
-    name:'Unity3d',
-    value:'Unity3d',
-    img:'Unity'
+    name: 'Unity3d',
+    value: 'Unity3d',
+    img: 'Unity'
   },
   {
-    name:'Egret',
-    value:'Egret',
-    img:'Egret'
+    name: 'Egret',
+    value: 'Egret',
+    img: 'Egret'
   },
   {
-    name:'UE4',
-    value:'UE4',
-    img:'UE4'
+    name: 'UE4',
+    value: 'UE4',
+    img: 'UE4'
   },
   {
-    name:'Cocos2dx-js',
-    value:'Cocos2dx-js',
-    img:'Cocos2dx'
+    name: 'Cocos2dx-js',
+    value: 'Cocos2dx-js',
+    img: 'Cocos2dx'
   },
   {
-    name:'Cocos2dx-lua',
-    value:'Cocos2dx-lua',
-    img:'Cocos2dx'
+    name: 'Cocos2dx-lua',
+    value: 'Cocos2dx-lua',
+    img: 'Cocos2dx'
   },
   {
-    name:'cocos-creator',
-    value:'cocos-creator',
-    img:'Cocos2dx'
+    name: 'cocos-creator',
+    value: 'cocos-creator',
+    img: 'Cocos2dx'
   },
   {
-    name:'Cocos2dx-c++',
-    value:'Cocos2dx-c++',
-    img:'Cocos2dx'
+    name: 'Cocos2dx-c++',
+    value: 'Cocos2dx-c++',
+    img: 'Cocos2dx'
   },
 ])
 let imgWidth = 0;
