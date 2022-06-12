5654edacdd2364f624718e433bfc90876b9a8c19
ZhouYixun <291028775@qq.com>
2022-06-12 23:51:04 +0800

    版本号纠正2

--- replace.sh
+++ replace.sh
@@ -1,3 +1,3 @@
 sed -i "s/SERVER_HOST/${SERVER_HOST}/g" /usr/share/nginx/html/assets/*.js
-sed -i "s/SONIC_GATEWAY_PORT/${SONIC_GATEWAY_PORT}/g" /usr/share/nginx/html/assets/*.js
+sed -i "s/SERVER_PORT/${SERVER_PORT}/g" /usr/share/nginx/html/assets/*.js
 /usr/sbin/nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
