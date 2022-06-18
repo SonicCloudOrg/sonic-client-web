aaacb977e578ebc5b068fa6b51b7efdf1344723f
July <22679824@qq.com>
2022-06-19 02:54:56 +0800

    feat: 添加验证Activity方法

--- src/components/StepShow.vue
+++ src/components/StepShow.vue
@@ -205,6 +205,10 @@ defineProps({
       <el-tag size="small" style="margin-right: 10px">验证标题</el-tag>
       期望值：{{ step.content }}
     </span>
+  <span v-if="step.stepType === 'getActivity'">
+      <el-tag size="small" style="margin-right: 10px">验证Activity</el-tag>
+      期望值：{{ step.content }}
+  </span>
   <span v-if="step.stepType === 'assertEquals'">
       <el-tag type="warning" size="small">断言验证(相等)</el-tag>
       真实值：{{ step.text }} 期望值：{{ step.content }}
