b91cb685aca6d4c4f8d5a8ef20af6ca78e3487b2
ZhouYixun <291028775@qq.com>
2022-10-08 20:58:58 +0800

    fix: Dockerfile2

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,6 @@ RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key
    && rm -rf /var/lib/apt/lists/* \
   && apt-get clean
 
-RUN google-chrome --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --no-sandbox --disable-dev-shm-usage --headless --disable-gpu
-
 COPY nginx.conf /etc/nginx/nginx.conf
 COPY dist/  /usr/share/nginx/html/
 COPY replace.sh /
--- replace.sh
+++ replace.sh
@@ -1,3 +1,4 @@
+nohup google-chrome --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --no-sandbox --disable-dev-shm-usage --headless --disable-gpu  >/dev/null 2>&1 &
 sed -i "s/SONIC_SERVER_HOST/${SONIC_SERVER_HOST}/g" /usr/share/nginx/html/assets/*.js
 sed -i "s/SONIC_SERVER_PORT/${SONIC_SERVER_PORT}/g" /usr/share/nginx/html/assets/*.js
 /usr/sbin/nginx -c /etc/nginx/nginx.conf -g 'daemon off;'
