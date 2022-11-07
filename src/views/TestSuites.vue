2e672fa11fa7939e67cd214ce7980b0d35c7588e
dongxiaoqiang <1193272767@qq.com>
2022-11-07 17:36:38 +0800

    fix:翻译bug

--- src/views/TestSuites.vue
+++ src/views/TestSuites.vue
@@ -117,7 +117,7 @@ onMounted(() => {
             >
             </el-table-column>
             <el-table-column
-                :label="$t('devices.from.model')"
+                :label="$t('devices.form.model')"
                 prop="model"
                 align="center"
             >
