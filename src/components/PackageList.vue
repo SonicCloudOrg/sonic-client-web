e919920d55988fbdd158f3c14296efb49f2ed724
fengbincao <fengbincao@tencent.com>
2023-05-08 14:12:57 +0800

    copy其他文件，第一行下面插入一下LICENSE协议

--- src/components/PackageList.vue
+++ src/components/PackageList.vue
@@ -1,4 +1,21 @@
 <script setup>
+/*
+ *   sonic-client-web  Front end of Sonic cloud real machine platform.
+ *   Copyright (C) 2022 SonicCloudOrg
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 import { ref, onMounted, watch } from 'vue';
 import { useI18n } from 'vue-i18n';
 import Pageable from './Pageable.vue';
