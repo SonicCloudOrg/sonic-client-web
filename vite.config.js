464f600a856968848610814f3e6cd9cd7b686f5c
YeungHoiChiu <1005935991@qq.com>
2024-08-08 20:46:37 +0800

    resolve: npm run build failure, set `chunkSizeWarningLimit` to 600KiB

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,32 @@
-import {defineConfig} from 'vite';
-import {join} from 'path';
+import { defineConfig } from 'vite';
+import { join } from 'path';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
-    server:{
-        port: 3002
-    },
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        }
-    },
-    resolve: {
-        alias: {
-            '@': join(__dirname, 'src'),
+  plugins: [vue()],
+  server: {
+    port: 3002,
+  },
+  build: {
+    chunkSizeWarningLimit: 600 * 1024, // 设置警告阈值为600KiB，因为element-plus超过500kb导致打包失败
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
         },
+      },
     },
+  },
+  resolve: {
+    alias: {
+      '@': join(__dirname, 'src'),
+    },
+  },
 });
