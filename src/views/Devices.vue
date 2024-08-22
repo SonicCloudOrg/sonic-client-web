352d6ef9e674f3af008b103a40b329f66d16b6c7
Eason <291028775@qq.com>
2024-08-22 10:11:38 +0800

    Update Devices.vue
    Signed-off-by: Eason <291028775@qq.com>

--- src/views/Devices.vue
+++ src/views/Devices.vue
@@ -69,8 +69,8 @@ const agentIds = ref([]);
 const cpus = ref([]);
 const sizes = ref([]);
 const isFlush = ref('0');
-const androidSystemVersion = ref([5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
-const iOSSystemVersion = ref([9, 10, 11, 12, 13, 14, 15, 16, 17]);
+const androidSystemVersion = ref([5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]);
+const iOSSystemVersion = ref([9, 10, 11, 12, 13, 14, 15, 16, 17, 18]);
 const harmonySystemVersion = ref([1, 2, 3, 4]);
 const manufacturer = ref([
   'APPLE',
